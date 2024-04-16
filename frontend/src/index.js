f299b67b83f4f0c83042223bbb74f28420402996
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-17 01:25:01 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
-import './output.css'
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
