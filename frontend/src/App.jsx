76ee407823dd5c19f10f7b1f671d1b8066611f0a
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-17 00:39:05 +0300

    Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/auth/SignUp";
 import SignIn from "./pages/auth/SignIn";
+import Home from "./pages/components/Home";
 
 const App = () => {
+  const token = localStorage.getItem("token");
+
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/register" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
-  </>
-);
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/register" element={<SignUp />} />
+        </Routes>
+        {token && (
+          <Routes>
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        )}
+      </BrowserRouter>
+    </>
+  );
 };
 
-export default App
+export default App;
